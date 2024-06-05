7ee49fb808452d1a8ac3f8f32a043b17338c8d7c
Vignita73 <leo73d@gmail.com>
2024-06-05 12:12:54 +0200

    oui

--- ville.js
+++ ville.js
@@ -2,4 +2,4 @@ const queryString = window.location.search;
 console.log(queryString);
 const urlParams = new URLSearchParams(queryString);
 const villeName = urlParams.get('villeName')
-villeName.log(product);
+villeName.log(villeName);
