d0fdbc92f15cff2cb897ca19d86e29be0686c1a2
Vignita73 <leo73d@gmail.com>
2024-06-11 14:38:21 +0200

    ui

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
-        ville: resolve(__dirname, "./ville.html"),
+        ville: resolve(__dirname, "ville.html"),
       },
     },
   },
